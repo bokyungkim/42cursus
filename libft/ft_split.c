e0d6373ab18babdc622210cd36c49f33f0801c4d
bokim <ohholyday@naver.com>
2021-05-07 19:32:02 +0900

    strtrim부터 하면 됨2

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/07 15:33:16 by bokim             #+#    #+#             */
-/*   Updated: 2021/05/07 19:18:34 by bokim            ###   ########.fr       */
+/*   Created: 2021/05/07 19:19:36 by bokim             #+#    #+#             */
+/*   Updated: 2021/05/07 19:21:06 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	**ft_split(char const *s, char c)
 {
-	char	*ptr;
-	int		first;
-	int		last;
 
-	if (s1 == 0)
-		return (0);
-	else if (set == 0)
-		return (ft_strdup(s1));
-	first = 0;
-	last = ft_strlen(s1);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 18:36:23 by bokim             #+#    #+#             */
-/*   Updated: 2021/05/07 14:51:37 by bokim            ###   ########.fr       */
+/*   Updated: 2021/05/07 19:20:59 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,7 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
