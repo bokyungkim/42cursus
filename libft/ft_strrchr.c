b8f24b3cce3804a2426ff8d7a5a41b2723aade4d
bokim <ohholyday@naver.com>
2021-05-12 03:41:33 +0900

    tester 돌리는 중

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 00:48:11 by bokim             #+#    #+#             */
-/*   Updated: 2021/05/12 03:12:44 by bokim            ###   ########.fr       */
+/*   Updated: 2021/05/12 03:34:38 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char	*ft_strrchr(const char *str, int c)
 	int		i;
 
 	if (!ft_strncmp(str, "", ft_strlen(str)))
-		return (0);
+		return ("");
 	i = ft_strlen(str) - 1;
 	if (c == '\0')
-        return ((char *)str + i + 1);
+		return ((char *)str + i + 1);
 	while (i >= 0)
 	{
 		if ((unsigned char)(str[i]) == (unsigned char)c)
 			return ((char *)(str + i));
 		i--;
 	}
-	return (0);
+	return (NULL);
 }
