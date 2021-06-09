5f648b479f33c2bfe38c0fe7bbaa7f33ca390240
Elly Kim <ellykim@Ellyui-MacBookAir.local>
2021-06-09 18:44:54 +0900

    gnl 중

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,18 +6,18 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 15:48:05 by bokim             #+#    #+#             */
-/*   Updated: 2021/05/18 15:55:01 by bokim            ###   ########.fr       */
+/*   Updated: 2021/06/09 18:43:58 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *find_newline(char *backup, char c)
+char    *find_newline(char *buff, char c)
 {
     char    *idx;
 
-    idx = backup;
-    while (idx)
+    idx = buff;
+    while (*idx)
 	{
         if (*idx == c)
             return (idx);
@@ -26,37 +26,42 @@ char    *find_newline(char *backup, char c)
     return (NULL);
 }
 
-int     cut_line(char **line, char **backup, char *newline_idx)
+int     cut_line(char **line, char **backup, char *buff, char *newline_ptr)
 {
-    *newline_idx = '\0';
-    if (!(*line = ft_strdup(*backup)))
+    *newline_ptr = '\0';
+    if (*backup)
+    {
+        if (!(*line = ft_strdup(*backup)))
+            return (-1);
+        *line = ft_strjoin(*line, buff);
+    }
+    else
+        if (!(*line = ft_strdup(buff)))
+            return (-1);
+    free(*backup);
+    *backup = newline_ptr + 1;
+    return (1);
+}
+
+int     return_all(char **line, char **backup, char *buff, int read_size)
+{
+    char    *newline_ptr;
+
+    if (read_size < 0)
+        return (-1);
+    if (*backup && (newline_ptr = find_newline(*backup, '\n')))
+        return (cut_line(line, backup, buff, newline_ptr));
+    if (*backup)
+    {
+        *line = *backup;
+        free(*backup);
+        *backup = NULL;
+    }
+    else if (!(*line = ft_strdup("")))
     {
         free(*backup);
         *backup = NULL;
         return (-1);
-    }
-    free(*backup);
-    *backup = newline_idx;
-    return (1);
-}
-
-int     return_all(char **line, char **backup, int read_size)
-{
-    char    *newline_idx;
-
-    if (read_size < 0)
-        return (-1);
-    if (*backup && (newline_idx = find_newline(*backup, '\n')))
-        return (cut_line(line, backup, newline_idx));
-    if (*backup)
-    {
-        *line = *backup;
-        *backup = NULL;
-    }
-    else
-    {
-        if (!(*line = ft_strdup("")))
-            return (-1);
     }
     return (0);  
 }
@@ -65,17 +70,18 @@ int		get_next_line(int fd, char **line)
 {
     static char     *backup[OPEN_MAX];
     char            buff[BUFFER_SIZE + 1];
-    char            *newline_idx;
+    char            *newline_ptr;
     int             read_size;
 
     if (fd < 0 || fd > OPEN_MAX || !line || BUFFER_SIZE <= 0)
         return (-1);
     while ((read_size = read(fd, buff, BUFFER_SIZE)) > 0)
     {
-        buff[fd] = '\0';
-        backup[fd] = ft_strjoin(backup[fd], buff);
-        if ((newline_idx = find_newline(backup[fd], '\n')))
-            return (cut_line(line, &backup[fd], newline_idx));
+        buff[read_size] = '\0';
+        if (backup[fd])
+            backup[fd] = ft_strjoin(backup[fd], buff);
+        if ((newline_ptr = find_newline(buff, '\n')))
+            return (cut_line(line, &backup[fd], buff, newline_ptr));
     }
-    return (return_all(line, backup, read_size));
+    return (return_all(line, backup, buff, read_size));
 }
