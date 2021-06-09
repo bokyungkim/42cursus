6fbc0af35481cea0ab9d2f4cef908458abdbc7c7
bokim <ohholyday@naver.com>
2021-06-09 23:27:34 +0900

    gnl

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,21 +2,18 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
-int	main(void)
+int main(void)
 {
-	char* line;
-	int	check;
-	int	fd;
-
+	char *line = 0;
+	int ret;
+	int fd;
 	fd = open("test.txt", O_RDONLY);
-	while ((check = get_next_line(fd, &line)) > 0)
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		printf("%s\n", line);
 		free(line);
-		line = NULL;
 	}
 	printf("%s\n", line);
 	free(line);
-	line = NULL;
 	return (0);
 }
