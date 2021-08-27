6a4ddb0f9838386913aeea075fd639792ad3cc9c
bokyungkim <43202470+bokyungkim@users.noreply.github.com>
2021-08-27 17:04:16 +0900

    0827

--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 22:40:07 by bokim             #+#    #+#             */
-/*   Updated: 2021/08/25 23:16:28 by bokim            ###   ########.fr       */
+/*   Updated: 2021/08/27 16:58:40 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,38 @@
 #include <string.h>
 #include "./so_long.h"
 
-int	main(void)
+void	error_end(void)
 {
-	printf("hi");
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
+}
+
+int	is_ber(char *file)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(file);
+	while (file[i] && file[i] != '.')
+	{
+		i++;
+		len--;
+	}
+	if (ft_strncmp(file + i, ".ber", len) == 0
+		|| ft_strncmp(file + i, ".BER", len) == 0)
+		return (1);
+	else
+		return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+	
+	if (argc != 2)
+		error_end();
+	if (is_ber(argv[1]) == 0)
+		error_end();
+	game = init_game();
 	return (0);
 }
--- so_long/srcs/so_long.h
+++ so_long/srcs/so_long.h
@@ -6,47 +6,48 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 22:39:56 by bokim             #+#    #+#             */
-/*   Updated: 2021/08/24 22:55:12 by bokim            ###   ########.fr       */
+/*   Updated: 2021/08/27 16:33:39 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
 
-# include "../mlx/mlx.h"
+#include "../mlx/mlx.h"
+#include "../libft/libft.h"
 
-# define X_EVENT_KEY_PRESS	2
-# define X_EVENT_KEY_RELEASE	3
-# define X_EVENT_KEY_EXIT	17
+#define X_EVENT_KEY_PRESS 2
+#define X_EVENT_KEY_RELEASE 3
+#define X_EVENT_KEY_EXIT 17
 
-# define KEY_ESC	53
-# define KEY_Q	12
-# define KEY_E	14
-# define KEY_W	13
-# define KEY_R	15
-# define KEY_A	0
-# define KEY_S	1
-# define KEY_D	2
+#define KEY_ESC 53
+#define KEY_Q 12
+#define KEY_E 14
+#define KEY_W 13
+#define KEY_R 15
+#define KEY_A 0
+#define KEY_S 1
+#define KEY_D 2
 
 typedef struct s_img
 {
-	void	*img;
-	int		*data;
+	void *img;
+	int *data;
 
-	int		size_l;
-	int		bpp;
-	int		endian;
-}	t_img;
+	int size_l;
+	int bpp;
+	int endian;
+} t_img;
 
-typedef struct	s_game
+typedef struct s_game
 {
-	void	*mlx;
-	void	*win;
-	t_img	player;
-	t_img	ground;
-	t_img	wall;
-	t_img	item;
-	t_img	door;
-}	t_game;
+	void *mlx;
+	void *win;
+	t_img player;
+	t_img ground;
+	t_img wall;
+	t_img item;
+	t_img door;
+} t_game;
 
 #endif
